b56d438d3848bf80fb00dff203ade4ff70593945
Kene Lin <kenelin@gmail.com>
2023-09-05 13:20:03 +0800

    Update VCRT policy

--- script/build-linux.bash
+++ script/build-linux.bash
@@ -104,8 +104,8 @@ fi
 
 
 ## Print build information
-echo "[$SYSTEM_PLATFORM] Project information: revision:" $PROJECT_REVISION
-echo "[$SYSTEM_PLATFORM] Project information: release type:" $PROJECT_RELEASE_TYPE
+echo "[$SYSTEM_PLATFORM] Project information: revision: $PROJECT_REVISION"
+echo "[$SYSTEM_PLATFORM] Project information: release type: $PROJECT_RELEASE_TYPE"
 echo "[$SYSTEM_PLATFORM] Project information: Disable clean build: $PROJECT_SHOULD_DISABLE_CLEAN_BUILD"
 echo "[$SYSTEM_PLATFORM] Project information: Net-SNMP with disabled apps: $PROJECT_NETSNMP_WITH_DISABLED_APPS"
 echo "[$SYSTEM_PLATFORM] Project information: Net-SNMP with disabled MIB loading: $PROJECT_NETSNMP_WITH_DISABLED_MIB_LOADING"
